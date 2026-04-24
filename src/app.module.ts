48a516ae419dec70bff6ddd885983683ae832fb1
akshay1ks <akshay1119@outlook.com>
2026-04-24 17:36:37 +0530

    Added db connection to Supabase and updated get and create api accordingly.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     TasksModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres1243',
-      database: 'task-management',
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
     }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: 'postgres1243',
+    //   database: 'task-management',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
   ],
 })
 export class AppModule {}
